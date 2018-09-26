b623ec65a0337576ce46bb418bfceeeffb99ef0f
laoliangliang <laoll@beikbank.com>
2018-09-26 15:38:13 +0800

    ip test

--- src/main/java/com/tzxylao/manager/controller/TestController.java
+++ src/main/java/com/tzxylao/manager/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -19,7 +20,7 @@ public class TestController {
 
     @RequestMapping("/ip")
     @ResponseBody
-    public String getIp() throws UnknownHostException {
+    public String getIp(HttpServletRequest request) throws UnknownHostException {
         InetAddress addr = InetAddress.getLocalHost();
         //获取本机ip
         String ip = addr.getHostAddress();
@@ -28,6 +29,12 @@ public class TestController {
         System.out.println(ip);
         System.out.println(hostName);
         log.info("ip:" + ip);
+        log.info("ip:" + request.getRemoteAddr());
+        log.info("X-Real-IP:{}", request.getHeader("X-Real-IP"));
+        InetAddress inetAddress = InetAddress.getByName(request.getRemoteAddr());
+        log.info("HostAddress:" + inetAddress.getHostAddress());
+        String auth = request.getHeader("Authorization");
+        log.info("Authorization:" + auth);
         return "ip:" + ip + "\n\r hostname:" + hostName;
     }
 }
